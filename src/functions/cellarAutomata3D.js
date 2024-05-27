b6eb6fc952b48a2ffdd29d3e81059ada128bfa52
Riccardo Scuto <riccardoscuto.96@gmail.com>
2024-05-27 18:26:20 +0200

    Working 3d Cellular

---
+++ src/functions/cellarAutomata3D.js
@@ -0,0 +1,181 @@
+export const generationPosition = (lato) => {
+    const toRet = [];
+    for (let j = 0; j < lato; j++) {
+        toRet[j] = []
+        let currentRow = 0;
+        for (let i = 0; i < lato * lato; i++) {
+            if (i % lato == 0) {
+                currentRow++;
+
+            }
+            toRet[j].push([i % lato * 2.1, currentRow * 2.1, j * 2.1])
+
+        }
+    }
+    return toRet;
+}
+
+export const generationMatrix = (lato) => {
+    const toRet = []
+    for (let j = 0; j < lato; j++) {
+        toRet[j] = []
+        for (let i = 0; i < lato; i++) {
+            toRet[j][i] = new Array(lato + 1).fill(false);
+        }
+    }
+    return toRet;
+}
+
+function getNeighborhood(x, y, z, lato) {
+    let e = null
+    let w = null
+    let n = null
+    let s = null
+    let t = null
+    let b = null
+    let ne = null
+    let se = null
+    let nw = null
+    let sw = null
+    let et = null
+    let eb = null
+    let wt = null
+    let wb = null
+    let nt = null
+    let nb = null
+    let st = null
+    let sb = null
+    let net = null
+    let neb = null
+    let set = null
+    let seb = null
+    let nwt = null
+    let nwb = null
+    let swt = null
+    let swb = null
+        // [1, 0, 0] (est) ok si
+        // [-1, 0, 0] (ovest) ok 
+        // [0, 1, 0] (nord)ok
+        // [0, -1, 0] (sud)ok
+        // [0, 0, 1] (sopra)ok
+        // [0, 0, -1] (sotto)ok
+        // [1, 1, 0] (nord-est) ok
+        // [1, -1, 0] (sud-est)ok
+        // [-1, 1, 0] (nord-ovest) ok
+        // [-1, -1, 0] (sud-ovest) ok 
+        // [1, 0, 1] (est sopra)ok
+        // [1, 0, -1] (est sotto)ok
+        // [-1, 0, 1] (ovest sopra)ok 
+        // [-1, 0, -1] (ovest sotto)ok 
+        // [0, 1, 1] (nord sopra)ok
+        // [0, 1, -1] (nord sotto)ok
+        // [0, -1, 1] (sud sopra)ok
+        // [0, -1, -1] (sud sotto)ok 
+        // [1, 1, 1] (nord-est sopra)ok
+        // [1, 1, -1] (nord-est sotto)ok
+        // [1, -1, 1] (sud-est sopra)ok
+        // [1, -1, -1] (sud-est sotto)ok
+        // [-1, 1, 1] (nord-ovest sopra)ok
+        // [-1, 1, -1] (nord-ovest sotto)ok
+        // [-1, -1, 1] (sud-ovest sopra)ok
+        // [-1, -1, -1] (sud-ovest sotto)ok
+    if (x != 0) w = [x - 1, y, z]; //si
+    if (x != 0 && y != 0) sw = [x - 1, y - 1, z];
+    if (x != 0 && y != lato - 1) nw = [x - 1, y + 1, z];
+    if (x != 0 && z != lato - 1) wt = [x - 1, y, z + 1]
+    if (x != 0 && z != 0) wb = [x - 1, y, z - 1]
+    if (x != 0 && y != lato - 1 && z != lato - 1) nwt = [x - 1, y + 1, z + 1]
+    if (x != 0 && y != lato - 1 && z != 0) nwb = [x - 1, y + 1, z - 1]
+    if (x != 0 && y != 0 && z != lato - 1) swt = [x - 1, y - 1, z + 1]
+    if (x != 0 && y != 0 && z != 0) swb = [x - 1, y - 1, z - 1]
+    if (x != lato - 1) e = [x + 1, y, z]; //ok
+    if (x != lato - 1 && y != lato - 1) ne = [x + 1, y + 1, z];
+    if (x != lato - 1 && y != 0) se = [x + 1, y - 1, z];
+    if (x != lato - 1 && y != lato - 1 && z != 0) neb = [x + 1, y + 1, z - 1];
+    if (x != lato - 1 && y != 0 && z != lato - 1) set = [x + 1, y - 1, z + 1];
+    if (x != lato - 1 && y != 0 && z != 0) seb = [x + 1, y - 1, z - 1];
+    if (y != 0) s = [x, y - 1, z]; //si
+    if (y != 0 && z != lato - 1) st = [x, y - 1, z + 1]
+    if (y != 0 && z != 0) sb = [x, y - 1, z - 1]
+    if (y != lato - 1) n = [x, y + 1, z]; //si
+    if (y != lato - 1 && z != lato - 1) nt = [x, y + 1, z + 1]
+    if (y != lato - 1 && z != 0) nb = [x, y + 1, z - 1]
+    if (z != 0) b = [x, y, z - 1]
+    if (z != lato - 1) t = [x, y, z + 1]
+    if (x != lato - 1 && z != lato - 1) et = [x + 1, y, z + 1];
+    if (x != lato - 1 && z != 0) eb = [x + 1, y, z - 1];
+    if (x != lato - 1 && z != lato - 1 && y != lato - 1) net = [x + 1, y + 1, z + 1];
+
+    return [e, w, n, s, t, b, ne, se, nw, sw, et, eb, wt, wb, nt, nb, st, sb, net, neb, set, seb, nwt, nwb, swt, swb];
+}
+
+function isLive(cell, matrix) {
+    //PRIAMA X E POI Y E POI Z (TECNOLOGIA 3d)
+    if (cell === null) {
+        return false;
+    }
+
+    return matrix[cell[0]][cell[1]][cell[2]];
+}
+export function simulation(x, y, z, matrix, lato) {
+    let numberAlive = 0;
+    for (let cell of getNeighborhood(x, y, z, lato)) {
+        if (isLive(cell, matrix)) {
+            numberAlive++;
+        }
+    }
+    const alive = isLive([x, y, z], matrix)
+    if (alive && numberAlive < 2) return false;
+    if (alive && (numberAlive == 3 || numberAlive == 2)) return true;
+    if (alive && numberAlive > 3) return false;
+    if (!alive && numberAlive == 3) return true;
+    return false;
+}
+export function currentPoint(matrix, index, lato) {
+    if (matrix.length < lato) return false;
+    console.log("sono un testo lungo", lato, index, "lungezza", matrix.length, index % lato, Math.floor(index / lato) % lato, Math.floor(index / (lato * lato)))
+
+    return matrix[index % lato][Math.floor(index / lato) % lato][Math.floor(index / (lato * lato))];
+
+}
+
+export function runSimulation(lato, Matrix) {
+    let newMatrix = []
+    for (let x = 0; x < lato; x++) {
+        newMatrix[x] = [];
+        for (let y = 0; y < lato; y++) {
+            newMatrix[x][y] = [];
+            for (let z = 0; z < lato; z++) {
+                newMatrix[x][y][z] = simulation(x, y, z, Matrix, lato);
+            }
+        }
+    }
+    return newMatrix
+}
+export function simulationCustomRule(x, y, z, matrix, lato, underpopulated, stable, birth, overpopulated) {
+    let numberAlive = 0;
+    for (let cell of getNeighborhood(x, y, z, lato)) {
+        if (isLive(cell, matrix)) {
+            numberAlive++;
+        }
+    }
+    const alive = isLive([x, y, z], matrix)
+    if (alive && numberAlive < underpopulated) return false;
+    if (alive && (numberAlive == stable || numberAlive == birth)) return true;
+    if (alive && numberAlive > overpopulated) return false;
+    if (!alive && numberAlive == birth) return true;
+    return false;
+}
+export function runSimulationCustom(lato, Matrix, underpopulated, stable, birth, overpopulated) {
+    let newMatrix = []
+    for (let x = 0; x < lato; x++) {
+        newMatrix[x] = [];
+        for (let y = 0; y < lato; y++) {
+            newMatrix[x][y] = [];
+            for (let z = 0; z < lato; z++) {
+                newMatrix[x][y][z] = simulationCustomRule(x, y, z, Matrix, lato, underpopulated, stable, birth, overpopulated);
+            }
+        }
+    }
+    return newMatrix
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,75 +5,157 @@ import { OrbitControls } from "@react-three/drei";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import Loader from '../components/Loader';
 import Cube from '../models/Cube';
-import { currentPoint, generationMatrix, generationPosition, runSimulation, simulation } from '../functions/cellarAutomata2D';
+import * as secondDimension from '../functions/cellarAutomata2D';
 import { useInterval } from '../hook/useInterval';
+import { currentPoint, generationMatrix, generationPosition, runSimulation, runSimulationCustom } from '../functions/cellarAutomata3D';
 
 
-const lato = 9;
+const lato = 20;
 const rendered = generationMatrix(lato);
-rendered[2][8] = true
-rendered[3][7] = true
-rendered[1][6] = true
-rendered[2][6] = true
-rendered[3][6] = true
+// rendered[1][1][1] = true
+// rendered[1][2][1] = true
+// rendered[1][3][1] = true
+// rendered[1][2][2] = true
+// rendered[1][2][0] = true
+// rendered[3][5][4] = true
+// rendered[3][6][3] = true
+// rendered[4][3][4] = true
+
+// // rendered[3][7] = true
+// // rendered[1][6] = true
+// // rendered[2][6] = true
+// // rendered[3][6] = true
 
 
-const positions = generationPosition(lato);
+// const positions = generationPosition(lato);
 
 const Home = () => {
     const [darkMode, setDarkMode] = useState(false);
     const toggleDarkMode = () => setDarkMode(!darkMode);
-    const [Matrix, setMatrix] = useState(rendered);
-    const [LastFrame, setLastFrame] = useState(Date.now())
+    const [Lato, setLato] = useState(2)
+    const [Positions, setPositions] = useState(generationPosition(Lato))
+    const [Matrix, setMatrix] = useState(generationMatrix(Lato));
+    const [Running, setRunning] = useState(false)
+    const [Birth, setBirth] = useState(3)
+    const [Underpopulated, setUnderpopulated] = useState(2)
+    const [Stable, setStable] = useState(2)
+    const [Overpulated, setOverpulated] = useState(3)
     useInterval(() => {
-        console.log("ciao")
-        const newMatrix = runSimulation(lato, Matrix);
-        setMatrix(newMatrix) 
-        console.log(newMatrix)
+        if (Running) {
+            console.log("ciao")
+            const newMatrix = runSimulationCustom(Lato, Matrix, Underpopulated, Stable, Birth, Overpulated);
+            setMatrix(newMatrix)
+            // console.log(newMatrix)
+        }
+
+    }, 100)
+    useEffect(() => {
+
+        const newPosition = generationPosition(Lato)
+        const newMatrix = []
+        for (let x = 0; x < Lato; x++) {
+            newMatrix[x] = []
+            for (let y = 0; y < Lato; y++) {
+                newMatrix[x][y] = []
+                for (let z = 0; z < Lato; z++) {
+                    newMatrix[x][y][z] = Math.floor(Math.random() * 10) % 6 == 0;
+                }
+            }
+        }
+        console.log("SONO BONACINI", newMatrix)
+        setMatrix(newMatrix)
+        console.log("SONO POSIZIONEAT" ,newPosition)
+        setPositions(newPosition)
 
 
-    }, 500)
+
+    }, [Lato])
 
 
-
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        const [u, s, b, o] = e.target.value.split(",");
+        setUnderpopulated(Number(u));
+        setStable(Number(s));
+        setBirth(Number(b));
+        setOverpulated(Number(o));
+    }
 
 
 
     const colors = [
         "red", "white", "blue", "yellow", "#324e2a",
-        "#1b3644", "#3d0079", "#ffbf6e", "brown"
+        "#1b3644", "#3d0079", "#ffbf6e", "brown", "cyan"
     ];
-    return (
-        <>
-            <button onClick={toggleDarkMode} style={{ position: 'absolute', zIndex: 2 }}>
-                {darkMode ? "Light Mode" : "Dark Mode"}
-            </button>
-            <Canvas
-                camera={{ position: [0, 0, 10], near: 0.1, far: 1000 }}
-                style={{ width: "100vw", height: "100vh", zIndex: "1", background: darkMode ? "black" : "white" }}
-            >
-                <Suspense fallback={<Loader />}>
-                    <directionalLight position={[1, 1, 1]} intensity={darkMode ? 0.5 : 2} />
-                    <ambientLight intensity={darkMode ? 0.2 : 0.5} />
-                    {darkMode ? null : <spotLight />}
-                    {darkMode ? null : <pointLight />}
-                    {darkMode ? null : <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />}
-                    {positions.map((position, index) => (
-                        <Cube key={index} position={position} color={colors[index]} darkMode={darkMode} isRendering={!currentPoint(Matrix, index, lato)} />
-                    ))}
-                    <OrbitControls />
-                    <EffectComposer>
-                        <Bloom
-                            luminanceThreshold={0}
-                            luminanceSmoothing={0.5}
-                            height={300}
-                            intensity={darkMode ? 2 : 0}
-                        />
-                    </EffectComposer>
-                </Suspense>
-            </Canvas>
-        </>
-    );
+    if (Lato && Matrix.length > 0)
+        return (
+            <><div style={{ position: 'absolute', zIndex: 2 }}>
+
+                <button onClick={toggleDarkMode} >
+                    {darkMode ? "Light Mode" : "Dark Mode"}
+                </button>
+                <button onClick={() => { runSimulationCustom(lato, Matrix, Underpopulated, Stable, Birth, Overpulated) }} >
+                    Step
+                </button>
+                <button onClick={() => { setUnderpopulated(Underpopulated + 1) }}>
+                    Underpopulate:{Underpopulated}
+                </button>
+                <button onClick={() => { setOverpulated(Overpulated + 1) }} >
+                    Overpopulated:{Overpulated}
+                </button>
+                <button onClick={() => { setStable(Stable + 1) }}>
+                    Stable:{Stable}
+                </button>
+                <button onClick={() => { setBirth(Birth + 1) }}>
+                    Birth:{Birth}
+                </button>
+                <button onClick={() => { setLato(Lato + 1) }} >
+                    Lato:{Lato}
+                </button>
+                <button onClick={() => { setRunning(!Running) }}>
+                    {Running ? "Pause" : "Start"}
+                </button>
+                <select defaultValue={undefined} onChange={handleChange}>
+                    <option value={[]}>
+                    </option>
+                    <option value={[2, 4, 2, 4]}>
+                        Rule 1:u2/o4/s2/b4
+                    </option>
+                    <option value={[1, 1, 1, 1]}>
+                        Rule 2:u1/o1/s1/b1
+                    </option>
+                    <option value={[4, 5, 4, 4]}>
+                        Rule 2:u4/o5/s4/b4
+                    </option>
+                </select>
+            </div>
+                <Canvas
+                    camera={{ position: [0, 0, 10], near: 0.1, far: 1000 }}
+                    style={{ width: "100vw", height: "100vh", zIndex: "1", background: darkMode ? "black" : "white" }}
+                >
+                    <Suspense fallback={<Loader />}>
+                        <directionalLight position={[1, 1, 1]} intensity={darkMode ? 0.5 : 2} />
+                        <ambientLight intensity={darkMode ? 0.2 : 0.5} />
+                        {darkMode ? null : <spotLight />}
+                        {darkMode ? null : <pointLight />}
+                        {darkMode ? null : <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />}
+                        {Positions.length>0 && Lato && Matrix.length>0 && Positions.flat(1).map((position, index) => (
+                            <Cube key={index} position={position} color={colors[index]} darkMode={darkMode} isRendering={currentPoint(Matrix, index, Lato)} />
+                        ))}
+                        {/* isRendering={!secondDimension.currentPoint(Matrix, index, lato)} */}
+                        <OrbitControls />
+                        <EffectComposer>
+                            <Bloom
+                                luminanceThreshold={0}
+                                luminanceSmoothing={0.5}
+                                height={300}
+                                intensity={darkMode ? 2 : 0}
+                            />
+                        </EffectComposer>
+                    </Suspense>
+                </Canvas>
+            </>
+        );
 };
 
 export default Home;
