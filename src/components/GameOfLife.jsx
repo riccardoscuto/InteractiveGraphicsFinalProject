7b6f6bb2ce5070d9e6141a85008750c2caae27fa
Riccardo Scuto <riccardoscuto.96@gmail.com>
2024-05-31 13:52:24 +0200

    Massive Refactor

---
+++ src/components/GameOfLife.jsx
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from 'react';
+
+import { OrbitControls} from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import Cube from '../models/Cube';
+// import * as sd from '../functions/cellarAutomata2D';
+import { useInterval } from '../hook/useInterval';
+import { currentPoint, generationMatrix, generationPosition, runSimulation, generationRandomMatrix } from '../functions/cellarAutomata';
+import { useFrame } from '@react-three/fiber';
+// import * as td  from "../functions/cellarAutomata3D"
+
+
+export const GameOfLife = ({ darkMode, wireframeMode, cellShadingMode,  speed, Running , Rule}) => {
+    const [Positions, setPositions] = useState(generationPosition(Rule.space, Rule.lato));
+    const [Matrix, setMatrix] = useState(generationMatrix(Rule.space, Rule.lato));
+    let lastIndex = null;
+    useEffect(() => {
+        let newMatrix;
+        let newPositions = generationPosition(Rule.space, Rule.lato);
+        console.log("pso", newPositions)
+        setPositions(newPositions)
+        if (Rule.matrixType == "fixed") {
+            newMatrix = generationMatrix(Rule.space, Rule.lato);
+            if(lastIndex == Rule.index){
+                translateM(Matrix, newMatrix);
+            }
+            if (Rule.space === "3D") {
+                for (let [x, y, z] of Rule.spawn) {
+
+                    newMatrix[x][y][z] = true;
+                }
+            }
+            if (Rule.space === "2D") {
+                for (let [x, y] of Rule.spawn) {
+
+                    newMatrix[x][y] = true;
+                }
+            }
+            lastIndex = Rule.index;
+        } else {
+            newMatrix = generationRandomMatrix(Rule.space, Rule.lato);
+        }
+        setMatrix(newMatrix);
+    }, [Rule])
+    let lastFrame = Date.now();
+    useFrame(() => {
+        if (Running && Date.now()- lastFrame> speed) {
+            console.log("ciao");
+            const newMatrix = runSimulation(Rule.space, Rule.lato, Matrix,Rule);
+            setMatrix(newMatrix);
+            lastFrame=Date.now();
+        }
+    });
+
+    // useEffect(() => {
+    //     const newPosition = generationPosition(Space, Lato);
+    //     const newMatrix = matrixType === "fixed" ? generationMatrix(Space, Lato) : generationRandomMatrix(Space, Lato);
+    //     translateM(Matrix, newMatrix);
+    //     setMatrix(newMatrix);
+    //     setPositions(newPosition);
+    // }, [Lato, matrixType]);
+
+    const translateM = (old, newM) => {
+        const l = old.length < newM.length ? old.length : newM.length;
+        if (Rule.space === "3D") {
+
+            for (let x = 0; x < l; x++) {
+                for (let y = 0; y < l; y++) {
+                    for (let z = 0; z < l; z++) {
+                        newM[x][y][z] = old[x][y][z];
+                    }
+                }
+            }
+        } else {
+            for (let x = 0; x < l; x++) {
+                for (let y = 0; y < l; y++) {
+                    newM[x][y] = old[x][y];
+                }
+            }
+        }
+    }
+    const generateRandomColor = () => {
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        return randomColor;
+    };
+
+    const getColor = (index) => {
+        if (Rule.colorMode === "random" && (!wireframeMode || (cubeColors && cubeColors.length > 0))) {
+            return generateRandomColor();
+        } else if (darkMode && wireframeMode && cubeColors && cubeColors.length > 0) {
+            return cubeColors[index];
+        } else {
+            return "red";
+        }
+    };
+
+    return (
+        <>
+            {/* <directionalLight position={[1, 1, 1]} intensity={darkMode ? 0.5 : 2} /> */}
+            {/* <ambientLight intensity={darkMode ? 0.2 : 0.5} /> */}
+            {darkMode ? null : <spotLight />}
+            {darkMode ? null : <pointLight />}
+            {darkMode ? null : <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />}
+            {Positions.length > 0 && Rule.lato && Matrix.length > 0 && Rule.space == "3D"
+                && Positions.flat(1).map((position, index) => (
+                    <Cube key={index}
+                        position={position}
+                        color={getColor(index)}
+                        darkMode={darkMode}
+                        isRendering={currentPoint(Rule.space, Matrix, Rule.lato, index)}
+                        cellShadingMode={cellShadingMode}
+                        wireframeMode={wireframeMode} />
+                ))}
+
+
+            {Positions.length > 0 && Rule.lato && Matrix.length > 0 && Rule.space == "2D"
+                && Positions.map((position, index) => (
+                    <Cube key={index}
+                        position={position}
+                        color={getColor(index)}
+                        darkMode={darkMode}
+                        isRendering={currentPoint(Rule.space, Matrix, Rule.lato, index)}
+                        cellShadingMode={cellShadingMode}
+                        wireframeMode={wireframeMode} />
+                ))}
+
+            <OrbitControls />
+            <EffectComposer>
+                <Bloom
+                    luminanceThreshold={0}
+                    luminanceSmoothing={0.5}
+                    height={300}
+                    intensity={darkMode ? 2 : 0}
+                />
+            </EffectComposer>
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ import { currentPoint, generationMatrix, generationPosition, runSimulation, gene
 // import * as td  from "../functions/cellarAutomata3D"
 import Rules from "../config/rules.json";
 import { Perf } from 'r3f-perf'
+import { GameOfLife } from '../components/GameOfLife';
 
 
 
@@ -19,115 +20,71 @@ import { Perf } from 'r3f-perf'
 
 const Home = () => {
     const [dpr, setDpr] = useState(0.5)
+    const [cubeColors, setCubeColors] = useState()
     const [darkMode, setDarkMode] = useState(false);
     const [cellShadingMode, setCellShadingMode] = useState(false);
     const [wireframeMode, setWireframeMode] = useState(false);
+    const [Running, setRunning] = useState(false);
+    const [sliderValue, setSliderValue] = useState(50);
+    const speed = 2000 - (sliderValue * 19);
     const [matrixType, setMatrixType] = useState("fixed");
     const [Space, setSpace] = useState("3D");
-    const [Lato, setLato] = useState(4);
-    const [Positions, setPositions] = useState(generationPosition(Space, Lato));
-    const [Matrix, setMatrix] = useState(generationMatrix(Space, Lato));
-    const [Running, setRunning] = useState(false);
+    const [Spawn, setSpawn] = useState([])
     const [Birth, setBirth] = useState(3);
     const [Underpopulated, setUnderpopulated] = useState(2);
     const [Stable, setStable] = useState(2);
     const [Overpopulated, setOverpopulated] = useState(3);
-    const [sliderValue, setSliderValue] = useState(50);
-    const [colorMode, setColorMode] = useState("random");
-    const [cubeColors, setCubeColors] = useState()
     const [Neigh, setNeigh] = useState("M");
-    const speed = 2000 - (sliderValue * 19);
+    const [Lato, setLato] = useState(4);
+    const [colorMode, setColorMode] = useState("random");
+    const [Rule, setRule] = useState({
+        lato: 4,
+        matrixType: "fixed",
+        space: "3D",
+        birth: 3,
+        underpopulated: 2,
+        stable: 2,
+        overpopulated: 3,
+        neigh: "M",
+        colorMode: "random",
+        spawn: [],
+    })
 
-    useInterval(() => {
-        if (Running) {
-            console.log("ciao");
-            const newMatrix = runSimulation(Space, Lato, Matrix, {
-                underpopulated: Underpopulated, stable: Stable, birth: Birth,
-                overpopulated: Overpopulated, neigh: Neigh
-            });
-            setMatrix(newMatrix);
-        }
-    }, speed);
-
-    useEffect(() => {
-        const newPosition = generationPosition(Space, Lato);
-        const newMatrix = matrixType === "fixed" ? generationMatrix(Space, Lato) : generationRandomMatrix(Space, Lato);
-        translateM(Matrix, newMatrix);
-        setMatrix(newMatrix);
-        setPositions(newPosition);
-    }, [Lato, matrixType]);
-
-    const translateM = (old, newM) => {
-        const l = old.length < newM.length ? old.length : newM.length;
-        if (Space === "3D") {
-
-            for (let x = 0; x < l; x++) {
-                for (let y = 0; y < l; y++) {
-                    for (let z = 0; z < l; z++) {
-                        newM[x][y][z] = old[x][y][z];
-                    }
-                }
-            }
-        } else {
-            for (let x = 0; x < l; x++) {
-                for (let y = 0; y < l; y++) {
-                    newM[x][y] = old[x][y];
-                }
-            }
-        }
+    const changeRule = (nameProp, valueProp) => {
+        const aux = Rule;
+        aux[nameProp] = valueProp;
+        setRule({ ...aux })
     }
+
 
     const handleChange = (e) => {
         // console.log(e.target.value);
 
-        const { birth, lato, overpopulated, spawn, stable, underpopulated, neigh, space ,starting} = Rules[e.target.value];
-        setLato(lato);
-        setUnderpopulated(underpopulated);
-        setStable(stable);
-        setBirth(birth);
-        setOverpopulated(overpopulated);
-        setSpace(space);
-        setNeigh(neigh);
-        setMatrixType(starting);
-        let newMatrix ;
-        let newPositions = generationPosition(space, lato);
-        console.log("pso", newPositions)
-        setPositions(newPositions)
-        if(starting =="fixed"){
-            newMatrix = generationMatrix(space, lato);
-
-            if (space === "3D") {
-                for (let [x, y, z] of spawn) {
-                    
-                    newMatrix[x][y][z] = true;
-                }
-            }
-            if (space === "2D") {
-                for (let [x, y] of spawn) {
-                    
-                    newMatrix[x][y] = true;
-                }
-            }
-        }else{
-            newMatrix =generationRandomMatrix(space, lato);
-        }
-        setMatrix(newMatrix);
+        const { birth, lato, overpopulated, spawn, stable, underpopulated, neigh, space, starting } = Rules[e.target.value];
+        // setLato(lato);
+        // setUnderpopulated(underpopulated);
+        // setStable(stable);
+        // setBirth(birth);
+        // setOverpopulated(overpopulated);
+        // setSpace(space);
+        // setNeigh(neigh);
+        // setMatrixType(starting);
+        // setSpawn(spawn);
+        setRule({
+            birth,
+            colorMode: Rule.colorMode,
+            lato,
+            matrixType: starting,
+            neigh,
+            overpopulated,
+            space,
+            spawn,
+            stable,
+            underpopulated, 
+            index:e.target.value
+        })
     }
 
-    const generateRandomColor = () => {
-        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        return randomColor;
-    };
-
-    const getColor = (index) => {
-        if (colorMode === "random" && (!wireframeMode || (cubeColors && cubeColors.length > 0))) {
-            return generateRandomColor();
-        } else if (darkMode && wireframeMode && cubeColors && cubeColors.length > 0) {
-            return cubeColors[index];
-        } else {
-            return "red";
-        }
-    };
 
     const colors = [
         "red", "white", "blue", "yellow", "#324e2a",
@@ -139,7 +96,7 @@ const Home = () => {
         }
     }, [wireframeMode]);
 
-    if (Lato && Matrix.length > 0)
+    if (Lato > 0)
         return (
             <>
                 <div style={{ position: 'absolute', zIndex: 2, top: 20, left: 20 }}>
@@ -160,7 +117,7 @@ const Home = () => {
                     <div style={{ marginBottom: 10 }}>
                         <label>
                             Matrix Type:
-                            <select value={matrixType} onChange={(e) => setMatrixType(e.target.value)}>
+                            <select value={Rule.matrixType} onChange={(e) => changeRule("matrixType", e.target.value)}>
                                 <option value="fixed">Fixed</option>
                                 <option value="random">Random</option>
                             </select>
@@ -168,7 +125,7 @@ const Home = () => {
                         </label>
                         <label>
                             Dimension:
-                            <select value={Space} onChange={(e) => setSpace(e.target.value)}>
+                            <select value={Rule.space} onChange={(e) => changeRule("space", e.target.value)}>
                                 <option value="3D">3D</option>
                                 <option value="2D">2D</option>
                             </select>
@@ -176,60 +133,60 @@ const Home = () => {
                         </label>
                         <label>
                             Neigh:
-                            <select value={Neigh} onChange={(e) => setNeigh(e.target.value)}>
+                            <select value={Rule.neigh} onChange={(e) => changeRule("neigh", e.target.value)}>
                                 <option value="M">M</option>
                                 <option value="VN">VN</option>
                             </select>
 
                         </label>
                         <label>
-                            Lato: {Lato}
+                            Lato: {Rule.lato}
                             <input
                                 type="range"
                                 min="1"
                                 max="30"
-                                value={Lato}
-                                onChange={(e) => setLato(Number(e.target.value))}
+                                value={Rule.lato}
+                                onChange={(e) => changeRule("lato", Number(e.target.value))}
                             />
                         </label>
                         <label>
-                            Underpopulate: {Underpopulated}
+                            Underpopulate: {Rule.underpopulated}
                             <input
                                 type="range"
                                 min="0"
                                 max="10"
-                                value={Underpopulated}
-                                onChange={(e) => setUnderpopulated(Number(e.target.value))}
+                                value={Rule.underpopulated}
+                                onChange={(e) => changeRule("underpopulated", Number(e.target.value))}
                             />
                         </label>
                         <label>
-                            Overpopulated: {Overpopulated}
+                            Overpopulated: {Rule.overpopulated}
                             <input
                                 type="range"
                                 min="0"
                                 max="10"
-                                value={Overpopulated}
-                                onChange={(e) => setOverpopulated(Number(e.target.value))}
+                                value={Rule.overpopulated}
+                                onChange={(e) => changeRule("overpopulated", Number(e.target.value))}
                             />
                         </label>
                         <label>
-                            Stable: {Stable}
+                            Stable: {Rule.stable}
                             <input
                                 type="range"
                                 min="0"
                                 max="10"
-                                value={Stable}
-                                onChange={(e) => setStable(Number(e.target.value))}
+                                value={Rule.stable}
+                                onChange={(e) => changeRule("stable", Number(e.target.value))}
                             />
                         </label>
                         <label>
-                            Birth: {Birth}
+                            Birth: {Rule.birth}
                             <input
                                 type="range"
                                 min="0"
                                 max="10"
-                                value={Birth}
-                                onChange={(e) => setBirth(Number(e.target.value))}
+                                value={Rule.birth}
+                                onChange={(e) => changeRule("birth", Number(e.target.value))}
                             />
                         </label>
                         <label>
@@ -244,7 +201,7 @@ const Home = () => {
                         </label>
                         <label>
                             Color Mode:
-                            <select value={colorMode} onChange={(e) => setColorMode(e.target.value)}>
+                            <select value={Rule.colorMode} onChange={(e) => changeRule("colorMode", e.target.value)}>
                                 <option value="random">Random</option>
                                 <option value="none">White</option>
                             </select>
@@ -262,49 +219,33 @@ const Home = () => {
                 </div>
 
                 <Canvas
-                
-                 dpr={dpr}
+
+                    dpr={dpr}
                     camera={{ position: [35, 10, 10], near: 0.1, far: 1000 }}
                     style={{ width: "100vw", height: "100vh", zIndex: "1", background: darkMode ? "black" : "white" }}
                 >      <Perf position="bottom-left" />
 
                     <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)} ></PerformanceMonitor>
                     <Suspense fallback={<Loader />}>
-                        {/* <directionalLight position={[1, 1, 1]} intensity={darkMode ? 0.5 : 2} /> */}
-                        {/* <ambientLight intensity={darkMode ? 0.2 : 0.5} /> */}
-                        {darkMode ? null : <spotLight />}
-                        {darkMode ? null : <pointLight />}
-                        {darkMode ? null : <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />}
-                        {Positions.length > 0 && Lato && Matrix.length > 0 && Space == "3D" && Positions.flat(1).map((position, index) => (
-                            <Cube key={index}
-                                position={position}
-                                color={getColor(index)}
-                                darkMode={darkMode}
-                                isRendering={currentPoint(Space, Matrix, Lato, index)}
-                                cellShadingMode={cellShadingMode}
-                                wireframeMode={wireframeMode} />
-                        ))}
+                        <GameOfLife
+                        Rule={Rule}
+                            Lato={Lato}
+                            Space={Space}
+                            cellShadingMode={cellShadingMode}
+                            darkMode={darkMode}
+                            wireframeMode={wireframeMode}
+                            colorMode={colorMode}
+                            Running={Running}
+                            matrixType={matrixType}
+                            Neigh={Neigh}
+                            Underpopulated={Underpopulated}
+                            Overpopulated={Overpopulated}
+                            Stable={Stable}
+                            Birth={Birth}
+                            speed={speed}
+                            Spawn={Spawn}
+                        />
 
-
-                        {Positions.length > 0 && Lato && Matrix.length > 0 && Space == "2D" && Positions.map((position, index) => (
-                            <Cube key={index}
-                                position={position}
-                                color={getColor(index)}
-                                darkMode={darkMode}
-                                isRendering={currentPoint(Space, Matrix, Lato, index)}
-                                cellShadingMode={cellShadingMode}
-                                wireframeMode={wireframeMode} />
-                        ))}
-
-                        <OrbitControls />
-                        <EffectComposer>
-                            <Bloom
-                                luminanceThreshold={0}
-                                luminanceSmoothing={0.5}
-                                height={300}
-                                intensity={darkMode ? 2 : 0}
-                            />
-                        </EffectComposer>
                     </Suspense>
                 </Canvas>
             </>
